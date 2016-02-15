ab160a749cbb62807db0dc564d53c8e96516e61a
A_Lippok <alippok@hotmail.co.uk>
2016-02-15 19:41:26 +0000

    Adds a simple user information request

--- st_helens.rb
+++ st_helens.rb
@@ -24,9 +24,21 @@ class Player
 
 end
 
-player_1 = Player.new("Jonny Lomax", "Full back", "176 cm", "88 kg")
-player_2 = Player.new("Atelea Vea", "Second row", "183 cm", "103 kg")
+player_1 = Player.new("Jonny Lomax", "full back", "176 cm", "88 kg")
+player_2 = Player.new("Atelea Vea", "second row", "183 cm", "103 kg")
 
 
 puts "Two players created:"
-puts "PLayer 1: #{player_1.name} plays at #{player_1.position}, and is #{player_1.height} tall and weighs #{player_1.weight}."
+puts "PLayer 1: #{player_1.name} plays at #{player_1.position}, is #{player_1.height} tall and weighs #{player_1.weight}."
+puts "PLayer 2: #{player_2.name} plays at #{player_2.position}, is #{player_2.height} tall and weighs #{player_2.weight}."
+
+puts "Please enter desired information: "
+request = gets.chomp
+
+if player_1.respond_to?(request.downcase)
+  puts player_1.send(request.downcase)
+else
+  puts "No such information available."
+end
+
+
