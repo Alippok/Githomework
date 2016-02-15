0587e435e80a521e00f4e5113504d30e06f099ba
A_Lippok <alippok@hotmail.co.uk>
2016-02-15 19:44:01 +0000

    Adds method defs to 2 other teams

---
+++ wigan_warriors.rb
@@ -0,0 +1,25 @@
+class Player
+  def initialize(name, position, height, weight)
+    @name = name
+    @position = position
+    @height = height
+    @weight = weight
+  end
+
+  def name
+    @name
+  end
+
+  def position
+    @position
+  end
+
+  def height
+    @height
+  end
+
+  def weight
+    @weight
+  end
+
+end
